5a37c60f4542c68161f617e130952918b35dfffa
faisa1khan <faisalkhan5464@gmail.com>
2019-10-26 13:17:15 +0530

    feat: initial commit

---
+++ src/pages/dashboard.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
+import { Router } from "@reach/router";
+import Layout from "../components/layout";
+import Profile from "../components/profile";
+import RoutBase from "../components/route-base";
+import RouteSecret from "../components/route-secret";
+import RouteLogin from "../components/route-login";
+
+const Dashboard = ({ location }) => {
+  useEffect(() => {
+    if (location.pathname.match(/^\/dashboard\/?$/)) {
+      navigate("/dashboard/login", { replace: true });
+    }
+  }, []);
+  return (
+    <Layout>
+      <Profile />
+      <Router>
+        <RoutBase path="/dashboard/base" />
+        <RouteSecret path="/dashboard/secret" />
+        <RouteLogin path="/dashboard/login" />
+      </Router>
+    </Layout>
+  );
+};
+
+export default Dashboard;
